31e8d76966d70c0e8205d3b4a93292c068b67f9e
alexferrante <36389437+alexferrante@users.noreply.github.com>
2018-12-18 14:05:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+/* uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -26,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({
   secret: 'secret'
 }));
-//
+*/
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
