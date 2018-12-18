aaaadd50be0157553801d75f218189995112615d
alexferrante <36389437+alexferrante@users.noreply.github.com>
2018-12-18 13:59:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var expressSession = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -29,7 +28,6 @@ app.use(expressSession({
 }));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
