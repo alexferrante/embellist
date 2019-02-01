326ec39bea662b229d5dda31bd5c656f4f5a4eba
alexferrante <alexferrante@github.com>
2019-02-01 14:36:48 -0500

    tweaking img generation

--- public/visuals.js
+++ public/visuals.js
@@ -24,33 +24,57 @@ if (playlist_id && accessToken) {
     // *unelegant* way of passing track ids for feature analysis 
     let track_ids = Cookies.get("track_ids");
     $.get({url: '/trackData', headers:{"Authorization": `Bearer ${accessToken}`}, data: {track_ids}}, function(data) {
-        
         var playlistEnergy = 0;
         var playlistMode = 0;
-        var playlistTempo = 0;
         var playlistValence = 0;
 
         for (i = 0; i < data.audio_features.length; i++) {
-            playlistEnergy += data.audio_features[i].energy * (1/data.audio_features.length);
-            playlistMode += data.audio_features[i].mode * (1/data.audio_features.length);
-            playlistValence += data.audio_features[i].valence * (1/data.audio_features.length);
-            playlistTempo += data.audio_features[i].tempo * (1/data.audio_features.length);
+            try {
+                playlistEnergy += data.audio_features[i].energy * (1/data.audio_features.length);
+                playlistMode += data.audio_features[i].mode * (1/data.audio_features.length);
+                playlistValence += data.audio_features[i].valence * (1/data.audio_features.length);
+            } catch (e) { } //pass over local files, which will have null feature values 
         }
         
-        
-        //determine starting hues (determined by modality)
+        console.log(playlistMode)
+        console.log(playlistEnergy)
+        console.log(playlistValence)
 
+
+        //determine hue component of HSL coloring (range of cold v. warm colors)
         if (playlistMode < .5) {
-            const R = playlistMode * (255 - 0);
-            const G = 1 - playlistMode * (255 - 0);
-            const B = 1 - playlistMode * (255 - 0);
+            var H = Math.floor(Math.random() * (283 - 63) + 63) //range of "cool" hue values
+        } else {
+            var H = Math.floor(Math.random() * (422 - 284) + 284)
+            if (H > 360) { H = H - 360}
+            //var H = //random number from 0 to 62 or 284 to 360 
         }
 
-        
-        playlistEnergy //determines saturation
-        playlistValence //determines lightness
+        //determine saturation component of HSL coloring 
+        var S = Math.floor(playlistEnergy * 100);
+        //determine lightness component of HSL coloring 
+        var L = Math.floor(playlistValence * 100);
 
-        // will probably need to move removal of cookies i.e. need playlist id for upload, but have to remove cookies in this script 
+        var c = document.createElement('canvas');
+        c.width = 300;
+        c.height = 300; 
+        var canvas = c.getContext("2d");
+        canvas.id = 'playlist_cover';
+        canvas.fillStyle = 'hsl('+H+','+S+'%,'+L+'%)';
+        canvas.fillRect(0, 0, 300, 300);
+        var i = c.toDataURL('image/png');
+        var img = document.createElement('img');
+        img.src = i;
+        img.style = 'display: block';
+        img.style = 'margin: 0 auto';
+        document.getElementById('view').appendChild(img);
+        
+    });
+  
+    $('#upload').click(function() {
+        $.get('/authorize', function(data) {
+          window.location = data;
+        });
         Cookies.remove("track_ids"); 
         Cookies.remove("playlist_id");
     });
