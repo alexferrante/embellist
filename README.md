3e5b55bd39a4970eb675422bc05ad8079d742151
alexferrante <36389437+alexferrante@users.noreply.github.com>
2020-07-22 15:03:54 -0700

    Force gh-page deployment

--- README.md
+++
@@ -1,3 +0,0 @@
-# embellist
-
-Web app that makes cover images for your Spotify playlists based on audio analysis of their contents
---
+++ precache-manifest.fea369799b3c3522753ff140593310a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75662e138bea0969e4e3652b08b79a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a13cce7f29be6b9430",
+    "url": "/static/css/main.c80d1e23.chunk.css"
+  },
+  {
+    "revision": "1beebf492cca95c731bd",
+    "url": "/static/js/2.eb10b1ad.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.eb10b1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a13cce7f29be6b9430",
+    "url": "/static/js/main.bcf3a23f.chunk.js"
+  },
+  {
+    "revision": "6b5fb1d83816de0b9125",
+    "url": "/static/js/runtime-main.4f8059e5.js"
+  },
+  {
+    "revision": "8d72101cad1547bed5ba3105041eeeae",
+    "url": "/static/media/LatoRegular.8d72101c.ttf"
+  }
+]);
